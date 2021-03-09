f8a0be710772fe8a60617b43aba3856ad308c6b9
Yash <58752969+imyash01@users.noreply.github.com>
2021-03-09 17:53:07 -0500

    fixed valgrind error

--- Word Wrapping/ww.c
+++ Word Wrapping/ww.c
@@ -76,7 +76,7 @@ int wrap(int width, int input_fd, int output_fd){
         for(int i = 0; i < SIZE; i++){ //make a while loop until '\n'
             char curr = read_buff[i];
 
-            if(read_buff[i-1] == 0 && read_buff[i] == 0)
+            if(i > 0 && (read_buff[i-1] == 0 && read_buff[i] == 0))
                 break; 
 
             //checks if curr is a space if is it it gets written and destroyed otherwise appended to the word.

6776008f59bc7ef0ea5afc57ac8315f7cf80681d
Neil D. Patel <ndp91@cray1.cs.rutgers.edu>
2021-03-10 18:19:14 -0500

    main

--- Word Wrapping/ww.c
+++ Word Wrapping/ww.c
@@ -144,7 +144,7 @@ int isdir(char *name) {
 	// should confirm err == 0
 	if (err) {
 		perror(name);  // print error message
-		return 0;
+		return -1;
 	}
 	
 	if (S_ISDIR(data.st_mode)) {
@@ -152,14 +152,17 @@ int isdir(char *name) {
 		// S_ISREG macro is true if the st_mode says the file is a regular file
 
 		return 1;
-	} 
+
+	} else if (S_ISREG(data.st_mode)) {
+        return 0;
+    } 
 	
-	return 0;
+	return -1;
 }
 
 int main (int argc, char* argv[] ) {
 
-    if(argv[1] < 1) return EXIT_FAILURE;
+   // if(argv[1] < 1) return EXIT_FAILURE;
 
     int width = atoi(argv[1]);
 
@@ -168,69 +171,73 @@ int main (int argc, char* argv[] ) {
          return EXIT_SUCCESS;
     }
 
-    
-    int fd = open(argv[2], O_RDONLY);
-    
-    if(fd == -1) {
-        return EXIT_FAILURE;
-    }
-    
-    
     //need to check if the argument given is a file or a directory
     // 2 scenarios
     
      
     //check to see if second argument is directory or file
     int check = isdir(argv[2]);
-    if(check == 1) {
-        wrap(fd, width, 1); //the second argument is a file, read from file and then display in std output : scenario 2
+    if(check == 0) {
+        int fd = open(argv[2], O_RDONLY);
+        wrap(width, fd, 1); //the second argument is a file, read from file and then display in std output : scenario 2
         return EXIT_SUCCESS;
-    } 
+        close(fd);
+    } else if(check == 1) { 
     
-    DIR *dirp = opendir(argv[2]);  // open the current directory
-    struct dirent *de;
+        DIR *dirp = opendir(argv[2]);  // open the current directory
+        struct dirent *de;
 
-    while ((de = readdir(dirp))) {
-        //puts(de->d_name);
-           //de->d_name;
+        while ((de = readdir(dirp))) {
+            //puts(de->d_name);
+            //de->d_name;
 
 
-        if(strcmp(de, "wrap" == 0)) { // have a for loop that checks first 5 letters
-            continue;
-        }
-        char *directory = "./";
-        strcat(directory, de->d_name);
-        
-        int check2 = isdir(directory);
-
-        // if file starts with wrap, skip over - ASK ABOUT THIS
-        if(de->d_name[0] == 'w' && de->d_name[1] == 'r' && de->d_name[2] == 'i' && de->d_name[3] == 't' && de->d_name[4] == 'e') {
-            continue;
-        }
-        //if file starts with . - idk need to ask about this
-
-        if(check2 == 1) { //this means we have a file 
-            char* temp1 = "./";
-            strcat(temp1, argv[2]);
-            int fd2 = open(temp1, O_RDONLY);
-
-            //create a new file
-            char* temp2 = "./";
-            strcat(temp2, argv[2]);
-            char* temp3 = "/wrap.";
-            strcat(temp3, de->d_name);
-            strcat(temp2, temp3);
-
-            //now write file, create the file
-            int fd3 = open(temp2, O_WRONLY|O_CREAT|O_APPEND);
-            wrap(fd2, width, fd3);
+            //if(strcmp(de->d_name, "wrap" == 0)) { // have a for loop that checks first 5 letters
+            //  continue;
+            //}
+            char *directory = "./";
+            strcat(directory, de->d_name);
             
-        } else if (check2 == 0) { //this means we have a directory and we just skip
-            continue;
+            int check2 = isdir(directory);
+
+            // if file starts with wrap, skip over - ASK ABOUT THIS
+            if(de->d_name[0] == 'w' && de->d_name[1] == 'r' && de->d_name[2] == 'a' && de->d_name[3] == 'p' && de->d_name[4] == '.') {
+                continue;
+            }
+            //if file starts with . - idk need to ask about this
+            if(de->d_name[0] == '.') {
+                continue;
+            }
+
+            if(check2 == 0) { //this means we have a file 
+                char* temp1 = "./";
+                strcat(temp1, argv[2]);
+                int fd2 = open(temp1, O_RDONLY);
+
+                //create a new file
+                char* temp2 = "./";
+                strcat(temp2, argv[2]);
+                char* temp3 = "wrap.";
+                strcat(temp3, de->d_name);
+                strcat(temp2, temp3);
+
+                //now write file, create the file
+                int fd3 = open(temp2, O_WRONLY|O_CREAT|O_APPEND);
+                wrap(fd2, width, fd3);
+
+                close(fd3);
+                close(fd2);
+                
+            } else if (check2 == 1) { //this means we have a directory and we just skip
+                continue;
+            }
         }
-    }
+        
 
     closedir(dirp); // should check for failure
+    
+    //close(fd);
+    }
     return EXIT_SUCCESS;
     
     

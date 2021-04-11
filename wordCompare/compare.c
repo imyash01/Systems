22536d7b196e7bb116918ec693b3a0b34c1020b6
Neil D. Patel <ndp91@cray1.cs.rutgers.edu>
2021-04-11 19:11:32 -0400

    edit

--- wordCompare/compare.c
+++ wordCompare/compare.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#define SIZE 256
 
 typedef struct node
 {
@@ -10,6 +12,11 @@ typedef struct node
     struct node *left;
     struct node *right;  
 } node;
+typedef struct {
+    size_t length;
+    size_t used;
+    char *data;
+} strbuf_t;
 
 node * makeNode(char* word){
     node *newNode = malloc(sizeof(node));
@@ -45,7 +52,87 @@ void toPrint(node * root){
     printf("%s\t", root->word);
     toPrint(root->right);
 }
+int sb_init(strbuf_t *L, size_t length)
+{
+    L->data = malloc(sizeof(char) * length);
+    if (!L->data) return 1;
+
+    L->length = length;
+    L->used   = 1;
+    L->data[0] = '\0';
+
+    return 0;
+}
+
+void sb_destroy(strbuf_t *L)
+{
+    free(L->data);
+}
+
+
+int sb_append(strbuf_t *L, char letter)
+{
+    if (L->used == L->length) {
+	size_t size = L->length * 2;
+	char *p = realloc(L->data, sizeof(char) * size);
+	if (!p) return 1;
+
+	L->data = p;
+	L->length = size;
+
+	//if (deBUG) printf("Increased size to %lu\n", size);
+    }
+
+    L->data[L->used-1] = letter;
+    L->data[L->used] = '\0';
+    ++L->used;
+
+    return 0;
+}
+
+int sb_concat(strbuf_t* list, char* str){
+    int i = 0;
+    while(str[i] != '\0'){
+        if(sb_append(list, str[i])){
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+}
+
+int tokenize(char* filePath) {
+   
+FILE *fp = fopen(filePath, "r");
+
+char temp;
+node *root = NULL;
+strbuf_t word;
+sb_init(&word, 32);
+while((temp = fgetc(fp)) != EOF ) {
+    //strbuf_t word;
+   // sb_init(&word, 32);
+    //node *root = NULL;
+    if(isalpha(temp) == 0) {
+        continue;
+    }
+    if(!isspace(temp) && word.used != 1) {
+        sb_append(&word, temp);
+
+    } else {
+        char *temp2 = malloc(sizeof(char) * word.used);
+        strcpy(temp2, word.data);
+        sb_destroy(&word);
+        sb_init(&word, 32);
+        root = toAdd(temp2, root);
+    }
+    
+}
+toPrint(root);
+return 0;
+}
 
 int main(int argc, char* argv[]){
-    return 0;
+    tokenize("file1.txt");
+
 }
